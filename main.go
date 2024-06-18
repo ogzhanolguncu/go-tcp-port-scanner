dcce1c1f149dd8dbb7353844295099599c0ec4ad
ogzhanolguncu <ogzhan11@gmail.com>
2024-06-18 12:48:58 +0300

    feat: add concurency

--- main.go
+++ main.go
@@ -3,56 +3,85 @@ package main
 import (
 	"flag"
 	"fmt"
+	"math"
 	"net"
+	"sync"
 	"time"
 )
 
-const TIMEOUT = 5
-const MAX_PORT = 65535
+const DEFAULT_TIMEOUT = time.Millisecond * 300
+const MAX_PORT = math.MaxUint16
+const DEFAULT_WORKER_COUNT = 100
+
+type Payload struct {
+	port       uint
+	host       string
+	timeout    uint
+	concurreny uint
+}
 
 func main() {
-	portPtr := flag.Uint("port", 0, "Target port")
-	hostPtr := flag.String("host", "", "Target host")
+	port := flag.Uint("port", 0, "Target port")
+	host := flag.String("host", "", "Target host")
+	// timeout := flag.Uint("timeout", 300, "Timeout in millis")
+	// concurrency := flag.Uint("concurrency", DEFAULT_WORKER_COUNT, "Number of concurrent workers")
 
 	flag.Parse()
+	// timeoutDuration := time.Millisecond * time.Duration(*timeout)
 
-	if *hostPtr == "" {
+	if *host == "" {
 		fmt.Println("Host is required.")
+		return
 	}
 
-	if *portPtr == 0 {
+	if *port == 0 {
 		fmt.Println("No port provided, performing a scan from 1 to 65535")
-
-		for port := uint(1); port <= MAX_PORT; port++ {
-			port, err := checkPort(*hostPtr, port)
-			if err == nil {
-				fmt.Printf("Port: %d is open\n", port)
-			}
-		}
+		portScanner(*host)
 	} else {
-		port, err := checkPort(*hostPtr, *portPtr)
+		port, err := checkPort(*host, *port)
 		if err == nil {
 			fmt.Printf("Port: %d is open\n", port)
 		} else {
 			fmt.Println("Error:", err)
 		}
 	}
-
 }
 
-/*
-Checks open ports for given host and port. If port is missing does a vanilla scan from 1-65536
-*/
+func portScanner(host string) {
+	var wg sync.WaitGroup
+	ports := make(chan uint, MAX_PORT)
+
+	// Launch worker goroutines
+	for i := 0; i < DEFAULT_WORKER_COUNT; i++ {
+		wg.Add(1)
+		go worker(host, ports, &wg)
+	}
+
+	// Send ports to the workers
+	for port := uint(1); port <= MAX_PORT; port++ {
+		ports <- port
+	}
+	close(ports)
+
+	// Wait for all workers to finish
+	wg.Wait()
+}
+
+func worker(host string, ports chan uint, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for port := range ports {
+		if _, err := checkPort(host, port); err == nil {
+			fmt.Printf("Port: %d is open\n", port)
+		}
+	}
+}
+
 func checkPort(host string, port uint) (uint, error) {
-	// Concatenated address: localhost:3333
 	address := fmt.Sprintf("%s:%d", host, port)
-
-	conn, err := net.DialTimeout("tcp", address, time.Second*TIMEOUT)
-
+	conn, err := net.DialTimeout("tcp", address, DEFAULT_TIMEOUT)
 	if err != nil {
 		return 0, err
 	}
-
 	conn.Close()
 	return port, nil
 }
